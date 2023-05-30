f380da719814b64c8aa722cd239acffa22ca4d7d
AhmedRefaatA <refaata788@gmail.com>
2023-05-30 23:07:38 +0300

    Add dashboard layout

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,13 @@
 <template>
-    <h1>Test Dashboard Page</h1>
-    <GoogleMaps></GoogleMaps>
+    <MainLayout>
+        <h1 class="bg-blue-400 text-3xl font-bold underline">Test Dashboard Page</h1>
+        <GoogleMaps></GoogleMaps>
+    </MainLayout>
+    
+    <!-- <h1 class="bg-blue-400 text-3xl font-bold underline">Test Dashboard Page</h1>
+    <GoogleMaps></GoogleMaps> -->
 </template>
 <script setup>
 import GoogleMaps from '@/Components/GoogleMaps.vue';
+import MainLayout from '../Layouts/MainLayout.vue';
 </script>
