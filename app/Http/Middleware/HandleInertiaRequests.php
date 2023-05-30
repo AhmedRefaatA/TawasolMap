21c260f2f5cd3fc0221b3860116d890aa17b79cb
AhmedRefaatA <refaata788@gmail.com>
2023-05-30 23:08:33 +0300

    update inertia middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -8,36 +8,49 @@ use Inertia\Middleware;
 class HandleInertiaRequests extends Middleware
 {
     /**
-     * The root template that's loaded on the first page visit.
+     * The root template that is loaded on the first page visit.
      *
-     * @see https://inertiajs.com/server-side-setup#root-template
      * @var string
      */
     protected $rootView = 'app';
 
     /**
-     * Determines the current asset version.
+     * Determine the current asset version.
      *
-     * @see https://inertiajs.com/asset-versioning
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
-    public function version(Request $request): ?string
+    public function version(Request $request)
     {
         return parent::version($request);
     }
 
     /**
-     * Defines the props that are shared by default.
+     * Define the props that are shared by default.
      *
-     * @see https://inertiajs.com/shared-data
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
-    public function share(Request $request): array
+    public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'auth' => [
+                'user' => $request->user(),
+                //'permissions' => $request->user()->getAllPermissions()->pluck('name')->toArray()
+            ],
+            'flash' => function () use ($request) {
+                return [
+                    'success' => $request->session()->get('success'),
+                    'error' => $request->session()->get('error'),
+                    'warning' => $request->session()->get('warning'),
+                ];
+            },
+            'showingMobileMenu' => false,
+            'data' => function () use ($request) {
+                return [
+                    'file_path' => $request->session()->get('file_path'),
+                ];
+            },
         ]);
     }
 }
